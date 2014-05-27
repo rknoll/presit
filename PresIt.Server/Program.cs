c0a9d6836c05685ac20537441fee4206dd5b82b9
Richard Knoll <r.knoll@me.com>
2014-05-27 15:54:09 +0200

    added doc to server project

--- PresIt.Server/Program.cs
+++ PresIt.Server/Program.cs
@@ -4,18 +4,16 @@ using PresIt.Service;
 
 namespace PresIt.Server {
     internal static class Program {
-        private static ServiceHost host;
-        private static AutoResetEvent close;
-
         /// <summary>
-        ///     The main entry point for the application.
+        ///     The main entry point for the server application.
         /// </summary>
         private static void Main() {
-            if (host != null) host.Close();
-            host = new ServiceHost(typeof (PresItService));
-            host.Open();
-            close = new AutoResetEvent(false);
+            // create the service
+            var host = new ServiceHost(typeof (PresItService));
+            host.Open(); // start the service
+            var close = new AutoResetEvent(false);
             host.Closed += (sender, args) => close.Set();
+            // wait until it is closed
             close.WaitOne();
         }
     }
